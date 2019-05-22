cbefea9e795abc3b6259ac3eb0d51e4f8fe205ec
Stacey Sharp <ssharpjr@gmail.com>
2019-05-22 14:38:16 -0400

    general updates

--- update_progs_lists.sh
+++ update_progs_lists.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # Update official repo packages
+echo Updating Official Arch Repos
 pacman -Qent | cut -d ' ' -f 1 > progs_official_repo.txt
+echo Total Official Packages:  $(cat progs_official_repo.txt | wc -l)
 
 # Update AUR packages
+echo Updating AUR Repos
 pacman -Qm | cut -d ' ' -f 1 > progs_aur.txt
+echo Total AUR Packages: $(cat progs_aur.txt | wc -l)
